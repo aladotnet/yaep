71ecc56a48376a0f97a4875d6e5fb3e97a138dd4
Ahmed Layachi <ahmed.layachi@outlook.com>
2022-12-11 14:23:37 +0100

    build process
    * nuke build process enhanced

--- build/Build.cs
+++ build/Build.cs
@@ -1,20 +1,52 @@
 using System;
+using System.IO;
 using System.Linq;
 using Nuke.Common;
+using Nuke.Common.ChangeLog;
 using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Coverlet;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitHub;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using ParameterAttribute = Nuke.Common.ParameterAttribute;
+using System.Threading.Tasks;
+using Octokit;
+using Octokit.Internal;
 
-[CheckBuildProjectConfigurations]
+[GitHubActions("build-test-publish",
+GitHubActionsImage.UbuntuLatest,
+AutoGenerate = true,
+FetchDepth = 0,
+    OnPushBranches = new[]
+    {
+        "main",
+        "dev",
+        "releases/**"
+    },
+    OnPullRequestBranches = new[]
+    {
+        "releases/**"
+    },
+    InvokedTargets = new[]
+    {
+        nameof(Pack),
+    },
+    EnableGitHubToken = true,
+    ImportSecrets = new[]
+    {
+        nameof(NuGetApiKey)
+    }
+)]
+
 [ShutdownDotNetAfterServerBuild]
 class Build : NukeBuild
 {
@@ -26,97 +58,192 @@ class Build : NukeBuild
 
     public static int Main() => Execute<Build>(x => x.Compile);
 
-
-    [Parameter("Nuget API key to publish the artifacts")]
+    [Parameter("Nuget Feed Url for Public Access of Pre Releases")]
+    readonly string NugetFeed;
+    [Parameter("Nuget API key to publish the artifacts"), Secret]
     readonly string NuGetApiKey;
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    [Parameter("Copyright Details")]
+    readonly string Copyright;
 
-    [Solution] readonly Solution Solution;
+    [Parameter("Artifacts Type")]
+    readonly string ArtifactsType;
+
+    [Parameter("Excluded Artifacts Type")]
+    readonly string ExcludedArtifactsType;
+
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
-
+    [GitVersion] readonly GitVersion GitVersion;
+    static GitHubActions GitHubActions => GitHubActions.Instance;
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+    AbsolutePath TestResultDirectory => OutputDirectory / "test-results";
+    AbsolutePath PackagesDirectory => OutputDirectory / "packages";
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    static readonly string PackageContentType = "application/octet-stream";
+    static string ChangeLogFile => RootDirectory / "CHANGELOG.md";
+    string GithubNugetFeed => GitHubActions != null
+      ? $"https://nuget.pkg.github.com/{GitHubActions.RepositoryOwner}/index.json"
+      : null;
 
     Target Clean => _ => _
         .Before(Restore)
+        .Description($"Cleaning.")
         .Executes(() =>
         {
             SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
             TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(OutputDirectory);
             EnsureCleanDirectory(ArtifactsDirectory);
         });
 
     Target Restore => _ => _
+        .Description($"Restoring Project Dependencies.")
+        .DependsOn(Clean)
         .Executes(() =>
-        {            
+        {
             DotNetRestore(s => s
-                .SetProjectFile(Solution));
+                .SetProjectFile(Solution.Extensions.CoreExtensions));
         });
 
     Target Compile => _ => _
+        .Description($"Building Project with the version.")
         .DependsOn(Restore)
         .Executes(() =>
         {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution)
+            DotNetBuild(_ => _
+                .SetProjectFile(Solution.Extensions.CoreExtensions)
                 .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
                 .EnableNoRestore());
         });
 
     Target Test => _ => _
-        .DependsOn(Compile)
-        .Executes(() =>
-        {
-            DotNetTest(s => s
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .EnableNoRestore()
-                .EnableNoBuild());
-        });
+    .DependsOn(Compile)
+    .Executes(() =>
+    {
+        DotNetTest(s => s
+            .SetProjectFile(Solution)
+            .SetConfiguration(Configuration)
+            .EnableNoRestore()
+            .EnableNoBuild());
+    });
 
     Target Pack => _ => _
-      .DependsOn(Clean, Compile,Test)
+      .Description($"Packing Project with the version.")
+      .Requires(() => Configuration.Equals(Configuration.Release))
+      .Produces(ArtifactsDirectory / ArtifactsType)
+      .Triggers(PublishToGithub, PublishToNuGet)
+      .DependsOn(Compile, Test)
       .Executes(() =>
       {
           DotNetPack(s => s
-              .SetProject(Solution.GetProject("CoreExtensions"))
+              .SetProject(Solution.Extensions.CoreExtensions)
               .SetOutputDirectory(ArtifactsDirectory)
-              .SetIncludeSymbols(true)
+              .SetNoBuild(SucceededTargets.Contains(Compile))
+              .SetRepositoryUrl(GitRepository.HttpsUrl)
+              .SetVersion(GitVersion.NuGetVersionV2)
               .SetConfiguration(Configuration)
-              .EnableNoRestore()
-              .EnableNoBuild());
+              .EnableNoBuild()
+              .EnableNoRestore());
       });
 
+    Target PublishToGithub => _ => _
+       .Description($"Publishing to Github for Development only.")
+       .Triggers(CreateRelease)
+       .Requires(() => Configuration.Equals(Configuration.Release))
+       .OnlyWhenStatic(() => GitRepository.IsOnDevelopBranch() || GitHubActions.IsPullRequest)
+       .Executes(() =>
+       {
+           GlobFiles(ArtifactsDirectory, ArtifactsType)
+               .Where(x => !x.EndsWith(ExcludedArtifactsType))
+               .ForEach(x =>
+               {
+                   DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(GithubNugetFeed)
+                       .SetApiKey(GitHubActions.Token)
+                       .EnableSkipDuplicate()
+                   );
+               });
+       });
+    Target PublishToNuGet => _ => _
+       .Description($"Publishing to NuGet with the version.")
+       .Triggers(CreateRelease)
+       .Requires(() => Configuration.Equals(Configuration.Release))
+       .OnlyWhenStatic(() => GitRepository.IsOnMainOrMasterBranch())
+       .Executes(() =>
+       {
+           GlobFiles(ArtifactsDirectory, ArtifactsType)
+               .Where(x => !x.EndsWith(ExcludedArtifactsType))
+               .ForEach(x =>
+               {
+                   DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(NugetFeed)
+                       .SetApiKey(NuGetApiKey)
+                       .EnableSkipDuplicate()
+                   );
+               });
+       });
 
+    Target CreateRelease => _ => _
+       .Description($"Creating release for the publishable version.")
+       .Requires(() => Configuration.Equals(Configuration.Release))
+       .OnlyWhenStatic(() => GitRepository.IsOnMainOrMasterBranch() || GitRepository.IsOnReleaseBranch())
+       .Executes(async () =>
+       {
+           var credentials = new Credentials(GitHubActions.Token);
+           GitHubTasks.GitHubClient = new GitHubClient(new ProductHeaderValue(nameof(NukeBuild)),
+               new InMemoryCredentialStore(credentials));
 
-    Target Publish => _ => _
-      .DependsOn(Pack)
-      .Requires(() => NuGetApiKey)
-      .Executes(() =>
-      {
-          (string feedUrl, string symbolsFeedUrl, string apiKey) = GetPublishTargetSettings();
-          var packageFile = GlobFiles(ArtifactsDirectory, "*.nupkg")
-                            .First(f => !f.EndsWith("symbols.nupkg"));
+           var (owner, name) = (GitRepository.GetGitHubOwner(), GitRepository.GetGitHubName());
 
-          DotNetNuGetPush(s => s
-              .SetTargetPath(packageFile)
-              .SetSource(feedUrl)
-              .SetSymbolSource(symbolsFeedUrl)
-              .SetApiKey(apiKey)
-          );
-      });
+           var releaseTag = GitVersion.NuGetVersionV2;
+           var changeLogSectionEntries = ChangelogTasks.ExtractChangelogSectionNotes(ChangeLogFile);
+           var latestChangeLog = changeLogSectionEntries
+               .Aggregate((c, n) => c + Environment.NewLine + n);
 
-    (string feedUrl, string symbolsFeedUrl, string apiKey) GetPublishTargetSettings()
-        => GitRepository.Branch switch
+           var newRelease = new NewRelease(releaseTag)
+           {
+               TargetCommitish = GitVersion.Sha,
+               Draft = true,
+               Name = $"v{releaseTag}",
+               Prerelease = !string.IsNullOrEmpty(GitVersion.PreReleaseTag),
+               Body = latestChangeLog
+           };
+
+           var createdRelease = await GitHubTasks
+                                       .GitHubClient
+                                       .Repository
+                                       .Release.Create(owner, name, newRelease);
+
+           GlobFiles(ArtifactsDirectory, ArtifactsType)
+              .Where(x => !x.EndsWith(ExcludedArtifactsType))
+              .ForEach(async x => await UploadReleaseAssetToGithub(createdRelease, x));
+
+           await GitHubTasks
+                      .GitHubClient
+                      .Repository
+                      .Release
+              .Edit(owner, name, createdRelease.Id, new ReleaseUpdate { Draft = false });
+       });
+
+    private static async Task UploadReleaseAssetToGithub(Release release, string asset)
+    {
+        await using var artifactStream = File.OpenRead(asset);
+        var fileName = Path.GetFileName(asset);
+        var assetUpload = new ReleaseAssetUpload
         {
-            "main" => (
-                "https://api.nuget.org/v3/index.json",
-                "https://api.nuget.org/v3/index.json",
-                NuGetApiKey),
-
-            _ => throw new InvalidOperationException($"Current branch \"{GitRepository.Branch}\" should not be publishing packages!")
+            FileName = fileName,
+            ContentType = PackageContentType,
+            RawData = artifactStream,
         };
-
+        await GitHubTasks.GitHubClient.Repository.Release.UploadAsset(release, assetUpload);
+    }
 }
