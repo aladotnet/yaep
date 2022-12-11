c0204cd4844407dbcc2292662d049c32ab690c51
Ahmed Layachi <ahmed.layachi@outlook.com>
2022-12-11 22:31:53 +0100

    xml extensions enhanced
    * added RootParent method

---
+++ tests/YAEP.Tests/XElementExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using System.Linq;
+using System.Xml.Linq;
+using Xunit;
+
+namespace YAEP.Tests
+{
+    public class XElementExtensionsTests
+    {
+        [Fact]
+        public void RootParent_returns_the_direct_ancestor()
+        {
+            var xml = @"<root><parent><child></child></parent></root>";
+
+            var doc = XElement.Parse(xml);
+
+            var child = doc.Descendants("child").Single();
+            
+            child.RootParent().Name.LocalName.Should().Be("root");
+        }
+
+        [Fact]
+        public void RootParent_given_no_ancestors_returns_null()
+        {
+            var xml = @"<root><parent><child></child></parent></root>";
+
+            var doc = XElement.Parse(xml);
+            
+            doc.RootParent().Should().BeNull();
+        }
+
+    }
+}
